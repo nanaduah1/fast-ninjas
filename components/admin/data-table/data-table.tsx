9cd01c69f9831003275500f84c99484cba5cc5e8
Nana Duah <gabano2005@gmail.com>
2023-05-15 17:56:43 -0400

    Expose Column type

--- components/admin/data-table/data-table.tsx
+++ components/admin/data-table/data-table.tsx
@@ -8,7 +8,7 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 
-type Column = {
+export type Column = {
   field: string;
   title: string;
   minWidth?: number;
