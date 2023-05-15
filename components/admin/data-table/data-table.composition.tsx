7ce04dcb4f18c024dff71df7a534a3b90a09ab3d
Lawrence Duah <59175088+nanaduah1@users.noreply.github.com>
2023-05-15 17:58:03 -0400

    Added performance improvements to data table (#4)
    * Added performance improvements to data table
    
    * Expose Column type

--- components/admin/data-table/data-table.composition.tsx
+++ components/admin/data-table/data-table.composition.tsx
@@ -1,15 +1,16 @@
 import React from "react"; //
 import { DataTable } from "./data-table";
 
+const row = [
+  { name: "Duah", address: "bn-34343", title: "Ninja 3" },
+  { name: "Rahman", address: "bx-34343", title: "Ninja 5" },
+  { name: "Kojo", address: "bz-34343", title: "Ninja 4" },
+];
+const col = [
+  { field: "name", title: "Full Name" },
+  { field: "title", title: "Job Title" },
+];
+
 export const BasicDataTable = () => {
-  const row = [
-    { name: "Duah", address: "bn-34343", title: "Ninja 3" },
-    { name: "Rahman", address: "bx-34343", title: "Ninja 5" },
-    { name: "Kojo", address: "bz-34343", title: "Ninja 4" },
-  ];
-  const col = [
-    { field: "name", title: "Full Name" },
-    { field: "title", title: "Job Title" },
-  ];
-  return <DataTable data={row} Columns={col} />;
+  return <DataTable data={row} columns={col} />;
 };
