680b4df90c8ded37437c242b93a1040c36a341d9
Rahman Lawal <rahmanabimbina@gmail.com>
2023-05-18 14:33:58 +0100

    only show pagination when there is available data

--- components/admin/data-table/data-table.composition.tsx
+++ components/admin/data-table/data-table.composition.tsx
@@ -1,11 +1,12 @@
 import React from "react"; //
 import { DataTable } from "./data-table";
 
-const row = [
-  { name: "Duah", address: "bn-34343", title: "Ninja 3" },
-  { name: "Rahman", address: "bx-34343", title: "Ninja 5" },
-  { name: "Kojo", address: "bz-34343", title: "Ninja 4" },
-];
+const row = [];
+// [
+//   { name: "Duah", address: "bn-34343", title: "Ninja 3" },
+//   { name: "Rahman", address: "bx-34343", title: "Ninja 5" },
+//   { name: "Kojo", address: "bz-34343", title: "Ninja 4" },
+// ];
 const col = [
   { field: "name", title: "Full Name" },
   { field: "title", title: "Job Title" },
